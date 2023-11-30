3bd5115707bfa2e5601fc828bd523a1845bd4f24
Kristian Suszczenia <you@example.com>
2023-11-30 17:25:51 +0000

    Edited function clarity and statistic presentation

--- functions/Functions_plot.R
+++ functions/Functions_plot.R
@@ -11,7 +11,7 @@
 ##
 ##----------------------------
 
-#A function to plot a histogram of a penguin species coloured by sex
+##A function to plot a histogram of a penguin species coloured by sex
 plot_histogram <- function(data, x, facet_by, title_as, x_axis_lab, y_axis_lab) {
   ggplot(data= data,
          aes_string(x= x,
@@ -25,7 +25,7 @@ plot_histogram <- function(data, x, facet_by, title_as, x_axis_lab, y_axis_lab) 
     theme_linedraw()
 }
 
-#Function to save figure
+##Function to save figure
 
 save_figure <- function(Figure_to_save,
                         filename, width, height, res, scaling){
@@ -40,9 +40,19 @@ save_figure <- function(Figure_to_save,
   dev.off()
 }
 
-#Function to plot a bar graph for body mass between sexes, with confidence intervals 
+##Function to plot a bar graph for body mass between sexes, with confidence intervals 
 
 plot_bar_BodyMass_Sex <- function(data) {
+  
+  #First we create a compatible object with out t-test statistics to later overlay the p value onto the graph
+  stat.test <- data %>%
+    t_test(body_mass_g ~ sex) %>%
+    add_significance()
+  
+  #This clarifies that the t test is testing between males and females
+  stat.test <- stat.test %>% add_xy_position(x = "sex")
+  
+  #Then we can use a bar graph plot function, including scatter with geom_jitter, and including error bars with the with stat_summary
   ggplot(data = data,
          aes(x = sex, y = body_mass_g), color = sex) +
     stat_summary(fun = "mean", geom = "bar", fill = c("lightpink", "lightblue"), show.legend = FALSE) +
@@ -53,16 +63,29 @@ plot_bar_BodyMass_Sex <- function(data) {
     labs(title = "Bar Graph of Body Mass by Sex in Gentoo",
          x = "Gentoo Sex",
          y = "Body Mass (g)")+
-    theme_bw()
+    theme_bw() +
+    
+    #then we add the t test results on top
+    stat_pvalue_manual(stat.test, label= "T-test, p = {p}") +
+    scale_y_continuous(expand = expansion(mult = c(0.05, 0.3))) 
+  
 }
 
-#Function to print mean, SE and confidence intervals for a given sex
+##Function to print mean, SE and confidence intervals for a given sex
 
 calculate_stats <- function(data, sex_selected) {
+  
+  #First we filter the data by either male or female, depending on what was called in the function 
   filter(Gentoo_only, sex == sex_selected) %>%
+    
+    #then we get the summary statistics we are interested in
     summarise(mean = mean(body_mass_g), SE = sd(body_mass_g) / sqrt(n()), CI = SE*2) %>%
     print()
 }
+
+
+
+
 
 
 
